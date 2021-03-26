f34be88c3baaaea008d31d74fadcc4e217925fee
kefeihu <kefei@nate.tech>
2021-03-26 19:32:14 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import os
+
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from stocks_info import get_stock_table_with_formatting
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates/")
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+@app.get('/')
+def welcome():
+    return 'welcome turtle'
+
+@app.get("/stocks")
+def get_stock_table(request: Request):
+    stock_table = get_stock_table_with_formatting()
+    return templates.TemplateResponse('stocks.html',
+                                      context={'request': request,'stock_table': stock_table})
